acb7e9a057084ac40205bbcc63f315b234ccaf2a
zeus2442 <fauzi.rgt128@gmail.com>
2024-06-23 19:46:45 +0700

    Update

--- src/components/Widget.vue
+++ src/components/Widget.vue
@@ -7,10 +7,10 @@
         <q-btn @click="fetchWeather" label="Get Weather" class="q-mt-md" />
       </q-card-section>
       <div class="custom-shape-divider-top-1719135224">
-    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
-    </svg>
-</div>
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+        </svg>
+      </div>
     </q-card>
 
     <q-card v-if="weather" class="my-card q-mt-md jjk">
@@ -24,41 +24,33 @@
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref, computed } from 'vue'
 import axios from 'axios'
 
-export default {
-  name: 'Widget',
-  data() {
-    return {
-      location: '',
-      weather: null
-    }
-  },
-  computed: {
-    localTime() {
-      if (this.weather) {
-        const offset = this.weather.timezone / 3600
-        const localTime = new Date(new Date().getTime() + offset * 3600 * 1000)
-        return localTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
-      }
-      return ''
-    }
-  },
-  methods: {
-    async fetchWeather() {
-      const apiKey = 'cafa6e92f105bddd4a4d17a0bf8a9a7e'
-      try {
-        const response = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${this.location}&appid=${apiKey}&units=metric`
-        )
-        this.weather = response.data
-      } catch (error) {
-        console.error('Error fetching weather data', error)
-      }
-    }
+const location = ref('')
+const weather = ref(null)
+
+const fetchWeather = async () => {
+  const apiKey = 'cafa6e92f105bddd4a4d17a0bf8a9a7e'
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location.value}&appid=${apiKey}&units=metric`
+    )
+    weather.value = response.data
+  } catch (error) {
+    console.error('Error fetching weather data', error)
   }
 }
+
+const localTime = computed(() => {
+  if (weather.value) {
+    const offset = weather.value.timezone / 3600
+    const localTime = new Date(new Date().getTime() + offset * 3600 * 1000)
+    return localTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+  }
+  return ''
+})
 </script>
 
 <style>
@@ -78,9 +70,11 @@ export default {
 .jjk {
   margin-top: 20px;
 }
-.text-h6{
+
+.text-h6 {
   font-weight: bold;
 }
+
 body {
   background: #0f2027;
   background: -webkit-linear-gradient(to right, #2c5364, #203a43, #0f2027);
@@ -88,26 +82,24 @@ body {
 }
 
 .custom-shape-divider-top-1719135224 {
-    position: absolute;
-    top: 1px;
-    left: 0;
-    width: 100%;
-    overflow: hidden;
-    line-height: 0;
-    z-index: -1;
-    filter: drop-shadow(1px 5px 10px rgb(231, 231, 231));
-
+  position: absolute;
+  top: 1px;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+  line-height: 0;
+  z-index: -1;
+  filter: drop-shadow(1px 5px 10px rgb(231, 231, 231));
 }
 
 .custom-shape-divider-top-1719135224 svg {
-    position: relative;
-    display: block;
-    width: calc(100% + 5px);
-    height: 150px;
+  position: relative;
+  display: block;
+  width: calc(100% + 5px);
+  height: 150px;
 }
 
 .custom-shape-divider-top-1719135224 .shape-fill {
-    fill: #347a98;
+  fill: #347a98;
 }
-
 </style>
